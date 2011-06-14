50dc2520d6ef2269720752e0619cbc35e4196c01
Philipp Haller <hallerp@gmail.com>
2011-06-14 11:07:17 -0700

    Added debug option for richer toString of SourceContext.

--- src/library/scala/reflect/SourceContext.scala
+++ src/library/scala/reflect/SourceContext.scala
@@ -1,6 +1,25 @@
+/*                     __                                               *\
+**     ________ ___   / /  ___     Scala API                            **
+**    / __/ __// _ | / /  / _ |    (c) 2007-2011, LAMP/EPFL             **
+**  __\ \/ /__/ __ |/ /__/ __ |    http://scala-lang.org/               **
+** /____/\___/_/ |_/____/_/ | |                                         **
+**                          |/                                          **
+\*                                                                      */
+
 package scala.reflect
 
+/** A SourceContext is a descriptor for the invocation of a method that takes
+ *  an implicit parameter of type SourceContext. It provides information about the
+ *  location in the source where the method is invoked, such as the source file name
+ *  and line number.
+ */
 trait SourceContext extends SourceLocation {
+
+  var parent: Option[SourceContext] =
+    None
+
+  def update(context: SourceContext): SourceContext
+
   def bindings: List[(String, Int)]
 
   def assignedVariable: Option[String] =
@@ -9,15 +28,22 @@ trait SourceContext extends SourceLocation {
 
   def methodName: String
 
-  def update(context: SourceContext): SourceContext
-
   def allContexts: List[List[(String, Int)]]
 
-  var parent: Option[SourceContext] =
-    None
+  override def toString = if (SourceContext.debug) {
+    "SourceContext at " + methodName + ":\n// " +
+    "Assigning to: " + assignedVariable + "\n// " +
+    "SourceLocation: " + super.toString + "\n// " +
+    "Parent: " +
+    (if (parent.isEmpty) "None" else "\n// " + parent.get.toString)
+  } else
+    super.toString
+
 }
 
 object SourceContext {
+
+  var debug = false
 
   def apply(name: String, sourceInfo: List[(String, Int)]): SourceContext =
     apply("<unknown file>", name, sourceInfo)

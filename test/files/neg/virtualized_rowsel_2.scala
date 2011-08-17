bf36af7b42bc6af974aaea738422207ee1d22c94
Adriaan Moors <adriaanm@gmail.com>
2011-08-17 17:23:25 +0200

    tests

---
+++ test/files/neg/virtualized_rowsel_2.scala
@@ -0,0 +1,11 @@
+object Test extends App {
+  trait Rep[T]
+  class MyRow extends Row[Rep]
+  class ApplyDynamicOps {
+    def applyDynamic[T](n: String)(as: AnyRef*): Rep[T] = error(n + as.mkString("(", ",", ")"))
+  }
+  implicit def applyDynamicOps[T <: MyRow](qual: Rep[T]): ApplyDynamicOps = new ApplyDynamicOps
+  
+  val qual = new MyRow{ val xxx: Rep[Int] = null }
+  val x: Rep[Int] = qual.xx // becomes `applyDynamicOps[MyRow{val xxx: Int}](qual).applyDynamic[Int]("xxx")()`
+}

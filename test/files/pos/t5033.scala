1853e003d994b341b0697aae8ac96cdd6fff774e
Adriaan Moors <adriaan.moors@epfl.ch>
2011-09-28 09:28:54 +0200

    more complete test case to make sure the multi-arglist case works as well

--- test/files/pos/t5033.scala
+++ test/files/pos/t5033.scala
@@ -1,11 +1,15 @@
+trait Eater {
+  type Food[T]
+}
+
 trait Fruit {
   type Seed
 }
 
 trait PipExtractor {
-  def extract(f: Fruit): f.Seed
+  def extract(a: Fruit)(b: Eater): b.Food[a.Seed]
 }
 
 trait LaserGuidedPipExtractor extends PipExtractor {
-  def extract(f: Fruit): f.Seed
+  def extract(f: Fruit)(g: Eater): g.Food[f.Seed]
 }
